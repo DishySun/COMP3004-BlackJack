cfd7da28c633f205333b5fe0397950855f6e31b8
DishySun <dishysun@hotmail.com>
2018-09-14 23:04:10 -0400

    added Deck class framework and its test class -Deck class is not yey implenmented -added some test cases in test class, cases may increase in the future

---
+++ src/main/java/blackjack/Deck.java
@@ -0,0 +1,20 @@
+package blackjack;
+import java.util.Stack;
+
+public class Deck {
+	private Stack<Card> deck;
+	
+	//constructor
+	public Deck() {}
+	
+	public int size() {return deck.size();}
+	
+	//deck initialization
+	//console: add all 52 card and shuffle
+	public void iniCDeck() {}
+	//file: read from a file
+	public void iniFDeck() {}
+	
+	public Card draw() {}
+	
+}
---
+++ src/test/java/blackjack/DeckTest.java
@@ -0,0 +1,30 @@
+package blackjack;
+import junit.framework.TestCase;
+
+public class DeckTest extends TestCase{
+	public void testConstructor() {
+		Deck testDeck = new Deck();
+		assertEquals(0, testDeck.size());
+	}
+	
+	public void testIniCDeck() {
+		Deck testDeck = new Deck();
+		testDeck.iniCDeck();
+		assertEquals(52, testDeck.size());
+	}
+	
+	public void testCDeckIsShuffled() {
+		Deck testDeck1 = new Deck();
+		Deck testDeck2 = new Deck();
+		testDeck1.iniCDeck();
+		testDeck2.iniCDeck();
+		int timesDrewTwoSameCard = 0;
+		for (int i = 0; i < 50; i++) {
+			Card c1 = testDeck1.draw();
+			Card c2 = testDeck2.draw();
+			if(c1.getSuit() == c2.getSuit() && c1.getRank() == c2.getRank())
+				timesDrewTwoSameCard++;
+		}
+		assertTrue(timesDrewTwoSameCard<10);
+	}
+}
