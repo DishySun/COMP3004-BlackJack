f35030c1f4aea30ddacc064390cb8f709b588b05
DishySun <dishysun@hotmail.com>
2018-09-16 01:39:07 -0400

    implemented toString method to Player class

--- src/main/java/blackjack/Player.java
+++ src/main/java/blackjack/Player.java
@@ -9,6 +9,20 @@ public class Player extends Participant{
 	}
 	
 	
-	public String toString() {}
+	public String toString() {
+		String result = "Player " + super.name +":\n";
+		result += "Hand";
+		if (super.getHands().size() > 1) result += " 1: ";
+		else result += ": ";
+		result += super.getFirstHand().toString();
+		if (super.findBestHand() == super.getFirstHand()) result += " (Highest)";
+		result +="\n";
+		if (super.getHands().size() > 1) {
+			result +="Hand 2: " + super.getSplitHand().toString();
+			if (super.findBestHand() == super.getSplitHand()) result += " (Highest)";
+			result +="\n";
+		}
+		return result;
+	}
 
 }
