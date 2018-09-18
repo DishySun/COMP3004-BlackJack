e57afb93ecd1a047c0822b8ddf1b3acb33d7de1f
DishySun <dishysun@hotmail.com>
2018-09-17 23:01:34 -0400

    implemented getChoice method for Dealer class

--- src/main/java/blackjack/Dealer.java
+++ src/main/java/blackjack/Dealer.java
@@ -12,7 +12,8 @@ public class Dealer extends Participant{
 		else currentHand = this.getFirstHand();
 		if (currentHand.getScore() < 17) return "H";
 		if (currentHand.getScore() > 17) return "S";
-		return null;
+		if (currentHand.isSoft17()) return "H";
+		else return "S";
 	}
 	public String toString() {
 		String result = "Dealer " + super.name +":\n";
