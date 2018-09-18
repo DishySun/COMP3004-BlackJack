384f537dd891bdac9708388399671a0a125c6852
DishySun <dishysun@hotmail.com>
2018-09-17 22:51:58 -0400

    add more test cases for getChoice in Dealer class

--- src/test/java/blackjack/DealerTest.java
+++ src/test/java/blackjack/DealerTest.java
@@ -28,4 +28,39 @@ public class DealerTest extends TestCase{
 		//score > 17
 		assertEquals("S", testDealer.getChoice());
 	}
+	public void testGetChoice3() {
+		//with splitting
+		Dealer testDealer = new Dealer();
+		testDealer.hit(new Card("D5"));
+		testDealer.hit(new Card("H5"));
+		//split
+		assertEquals("D", testDealer.getChoice());
+		
+		testDealer.split(new Card("CA"), new Card("C8"));
+		//current on hand1 (D5 CA = 16 which < 17) will hit on hand 1
+		assertEquals("H", testDealer.getChoice());
+		assertEquals(16, testDealer.getFirstHand().getScore());
+		
+		testDealer.hit(new Card("SA"));
+		//current on hand1 (D5 CA SA Soft 17 with 2 Aces) will hit on hand 1
+		assertEquals("H", testDealer.getChoice());
+		assertEquals(17, testDealer.getFirstHand().getScore());
+		
+		testDealer.hit(new Card("S7"));
+		//hand 1 (D5 CA SA S7 = 14 < 17) will hit on hand 1 
+		assertEquals("H", testDealer.getChoice());
+		assertEquals(14, testDealer.getFirstHand().getScore());
+		
+		testDealer.hit(new Card("HK"));
+		//hand 1(D5 CA SA S7 HK = 24)
+		assertEquals(24, testDealer.getFirstHand().getScore());
+		
+		//dealer will work on hand 2 automatically
+		//current on hand 2(H5 C8 = 13 < 17) will hit on hand2
+		assertEquals("H", testDealer.getChoice());
+		testDealer.hit(new Card("S8"));
+		//hand 2 (H5 C8 S8 = 21 > 17) will stand on hand 2
+		assertEquals(21, testDealer.getSplitHand().getScore());
+		assertEquals("S", testDealer.getChoice());
+	}
 }
